48f2ae9ffe719986e1f26947c287dbd466308fb4
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2023-11-10 11:25:45 +0100

    feat: fine

---
+++ snack-2/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const promUno = prompt('Inserisci la prima parola');
+const promDue = prompt('Inserisci la seconda parola');
+
+const lunghezzaUno = promUno.length;
+const lunghezzaDue = promDue.length;
+
+if (lunghezzaUno > lunghezzaDue) {
+  console.log(promUno);
+  console.log(promDue);
+} else if (lunghezzaUno < lunghezzaDue) {
+  console.log(promDue);
+  console.log(promUno);
+} else if (lunghezzaUno === lunghezzaDue) {
+  console.log(promUno);
+  console.log(promDue);
+}
